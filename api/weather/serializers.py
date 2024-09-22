efde753e2c4d7adaa2b0b7d02841ad658e4f9a16
AlexRiosXI <alexriosxi@gmail.com>
2024-09-22 16:19:14 -0600

    feat(serializers): add serializers to validate weather endpoint params

---
+++ api/weather/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+import re
+
+class WeatherRequestSerializer(serializers.Serializer):
+    q = serializers.CharField(max_length=100, required=True)        
+    page = serializers.IntegerField(default=1)
+    per_page = serializers.IntegerField(default=10)
+    show_all = serializers.BooleanField(default=False)
+
+    def  validate_q(self, value):
+        if len(value) > 100:
+            raise serializers.ValidationError("Destination param must be less than 100 characters")
+        
+    def validate_page(self, value):
+        if value < 1:
+            raise serializers.ValidationError("Page param must be greater than 0")
+        
+    def validate_per_page(self, value):
+        if value < 1:
+            raise serializers.ValidationError("Per_page param must be greater than 0")
+        
+    def validate_show_all(self, value):
+        if not isinstance(value, bool):
+            raise serializers.ValidationError("Show_all param must be a boolean")
+            
+    
+    
--- api/weather/views.py
+++ api/weather/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from weather.serializers import WeatherRequestSerializer
+
 # Create your views here.
 class WeatherAPIHealthView(APIView):
     def get(self, request):
@@ -10,5 +12,16 @@ class WeatherAPIHealthView(APIView):
 
 class WeatherAPIView(APIView):
     def get(self, request):
-        print("get")
+        serializer = WeatherRequestSerializer(data=request.GET)
+        
+        if not serializer.is_valid():
+            if "q" in serializer.errors:
+                print(serializer.errors)
+                return Response({"status": "destination error", "message": serializer.errors["q"][0]}, status=status.HTTP_400_BAD_REQUEST)
+            if "page" in serializer.errors:
+                return Response({"status": "page error", "message": serializer.errors["page"][0]}, status=status.HTTP_400_BAD_REQUEST)
+            if "per_page" in serializer.errors:
+                return Response({"status": "per_page error", "message": serializer.errors["per_page"][0]}, status=status.HTTP_400_BAD_REQUEST)
+            if "show_all" in serializer.errors:
+                return Response({"status": "show_all error", "message": serializer.errors["show_all"][0]}, status=status.HTTP_400_BAD_REQUEST)
         return Response({"status": "ok"}, status=status.HTTP_200_OK)

6cb6cdfa42de4bbbc80c622e5bb6c14f7afbd932
AlexRiosXI <alexriosxi@gmail.com>
2024-09-22 15:25:17 -0600

    test(health): add unit test for health endpoint

--- api/weather/tests.py
+++ api/weather/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from django.urls import reverse
+from rest_framework import status
 # Create your tests here.
+
+WEATHER_HEALTH = reverse("weather-api-health")
+
+class WeatherTestCase(TestCase):
+    def test_weather_endpoint_health(self):
+        response = self.client.get(WEATHER_HEALTH)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)

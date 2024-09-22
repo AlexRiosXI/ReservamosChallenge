58441c668e1b8f932106f5254dd5a2dcacbe6642
AlexRiosXI <alexriosxi@gmail.com>
2024-09-22 15:25:36 -0600

    feat(api): implement health check endpoint for application status verification

--- api/api/urls.py
+++ api/api/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from weather import views as WeatherViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/health", WeatherViews.WeatherAPIHealthView.as_view(), name="weather-api-health"),
 ]
